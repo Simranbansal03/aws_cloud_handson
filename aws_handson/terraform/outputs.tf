7d101273c2e67a8bea067a04c22bd6ebbdb38fd8
simran bansal <90952972+Simranbansal03@users.noreply.github.com>
2025-11-25 05:01:52 +0000

    Add separated Terraform files for S3 bucket

--- aws_handson/terraform/outputs.tf
+++ aws_handson/terraform/outputs.tf
@@ -1,7 +1,9 @@
+
 output "bucket_name" {
-    value = aws_s3_bucket.bucket.bucket
-  
-}output "region" {
+  value = aws_s3_bucket.bucket.bucket
+}
+
+output "region" {
   description = "The AWS region used for deployment."
   value       = var.aws_region
 }
